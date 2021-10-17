f5a3b38196defde78c15c147d138254a598a635f
fayeyoussou <fayeyousso@gmail.com>
2021-10-17 14:28:25 +0000

    basics-05-styling

--- src/views/header.js
+++ src/views/header.js
@@ -1,11 +1,27 @@
 import React,{Component} from "react";
 // or we can add React.Component directly
 class Header extends Component {
+    /* style = {
+        header : {
+            background:"#a15599",
+
+        },
+        logo : {
+            fontFamily :'Festive',
+            color: '#fff',
+            textAlign : 'Center',
+            fontSize : '70px',
+            textTransform : 'capitalize'
+        }
+    }; */
+    // not a good way to do styling
     render()
     {
-        return <>
-            the date is {new Date().getFullYear()}
-        </>
+        //using classname is the best way of style
+        return <header className="logo">
+            <div>logo</div>
+            <div>the date is {new Date().getFullYear()}</div>
+        </header>
     }
 }
 export default Header;
