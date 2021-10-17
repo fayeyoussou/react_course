0b1ce0d5dfa4caaf57835c46838ec012ee002e42
fayeyoussou <fayeyousso@gmail.com>
2021-10-17 12:57:45 +0000

    Basics-02

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import react from "react";//start a react application
-import ReactDOM from 'react-dom';// react help connect the dom with react application
-const App = () => {
-    return  (
-        <div>
-            <h1>hello</h1>
-        </div>
-    )//it look like html but its Jsx
-}
-// if return something its gonna be a react component
-// so you always need to return some code
-//component should always start with a capital A
+import React from "react";
+// import react from "react";
+import ReactDOM from 'react-dom';
+const App = () => (
+    <>
+        <h1>ceci est le premier</h1>
+        <h2>ceci est le second</h2>
+    </>
+)
+/**
+ * / return  React.createElement('h1',{className:'title'},React.createElement('div',{},'Voila'))
+ * this can be done but it is too complicate and we dont want complication
+ */ 
+    
 ReactDOM.render(<App/>,document.querySelector('#root'))
-// <App/> tell react that app function is a component function
